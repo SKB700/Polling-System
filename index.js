0fcb611b423da88cebb9a1f0ea51512725e2969b
SKB700 <147979270+SKB700@users.noreply.github.com>
2023-12-23 11:27:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
 const express = require('express');
-const connectWithDb = require('./config/db');
-require('dotenv').config();
-const app = express();
-const { PORT } = process.env;
+const router = express.Router();
 
-// for swagger documentation
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const homeController = require('../controllers/home_controller');
 
-// connect with database
-connectWithDb();
+router.get('/', homeController.home);
+router.use('/questions', require('./questions'))
+router.use('/options',require('./options'))
 
-// regular middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// use express router
-app.use('/', require('./routes'));
-
-app.listen(PORT || 5000, (err) => {
-  if (err) {
-    console.log(`Error in running the server : ${err}`);
-  }
-  console.log(`Server is up and running at ${PORT}`);
-});
+module.exports = router;
